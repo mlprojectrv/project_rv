1c124799d62c49bf2ead5543c091c29bf66f5d8c
AkivaKT <tun18001@byui.edu>
2019-03-22 13:47:51 -0600

    good

--- project_data_wrangle.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-library(readr)
-
-dat <- read_csv('https://raw.githubusercontent.com/propublica/compas-analysis/master/compas-scores-two-years.csv')
-
-
-dat_n <- dat %>% 
-  select()
