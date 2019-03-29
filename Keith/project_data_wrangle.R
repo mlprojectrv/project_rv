e269a4d5ee5fc9b410e6ffd12242bbb92fb78e87
AkivaKT <tun18001@byui.edu>
2019-03-29 14:25:27 -0600

    updates

--- Keith/project_data_wrangle.R
+++ Keith/project_data_wrangle.R
@@ -1,8 +1,14 @@
+
+################################library##############################################
 library(tidyverse)
 library(readr)
+lib
 
+################################Read File############################################
 dat <- read_csv('https://raw.githubusercontent.com/propublica/compas-analysis/master/compas-scores-two-years.csv')
 
 
+################################data wrangle#########################################
 dat_n <- dat %>% 
-  select()
+  select(sex,  age, age_cat, race, juv_fel_count, juv_misd_count, juv_other_count, priors_count, c_charge_degree,
+         c_charge_desc, is_recid, two_year_recid)
