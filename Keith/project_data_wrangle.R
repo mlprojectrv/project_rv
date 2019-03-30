98b1e468eed3138da3c4a124bd3b3144a473c30f
AkivaKT <tun18001@byui.edu>
2019-03-30 16:20:16 -0600

    updates

--- Keith/project_data_wrangle.R
+++ Keith/project_data_wrangle.R
@@ -2,7 +2,6 @@
 ################################library##############################################
 library(tidyverse)
 library(readr)
-lib
 
 ################################Read File############################################
 dat <- read_csv('https://raw.githubusercontent.com/propublica/compas-analysis/master/compas-scores-two-years.csv')
@@ -10,5 +9,29 @@ dat <- read_csv('https://raw.githubusercontent.com/propublica/compas-analysis/ma
 
 ################################data wrangle#########################################
 dat_n <- dat %>% 
-  select(sex,  age, age_cat, race, juv_fel_count, juv_misd_count, juv_other_count, priors_count, c_charge_degree,
-         c_charge_desc, is_recid, two_year_recid)
+  select(sex,  
+         age, 
+         age_cat, 
+         race, 
+         juv_fel_count,
+         juv_misd_count, 
+         juv_other_count, 
+         priors_count, 
+         c_charge_degree,
+         c_charge_desc,
+         is_recid, 
+         two_year_recid)
+
+c_cu <- dat_n$c_charge_desc %>% 
+  unique()
+
+c_charge <- tibble(c_charge_desc = c_cu) %>% 
+  mutate(index = row_number())
+
+dat_n %>% 
+  left_join(c_charge) %>% 
+  mutate(c_charge_desc = )
+
+
+
+
